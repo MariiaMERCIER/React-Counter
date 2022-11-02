0e12edcc133501532b3f3f3c76083a8189437b8b
Mariia IVANCHENKO <mariiaivanchenko@mbp-de-mariia.home>
2022-11-02 15:44:08 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Vector from "./assets/images/Vector.png";
+import { useState } from "react";
+import Plus from "./assets/images/Plus.png";
+import Moins from "./assets/images/Moins.png";
 
-function App() {
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  const reset = () => {
+    setCounter(0);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="header">
+        <img className="logo" src={Vector} alt="logo" />
+        <h1> React Counter</h1>
       </header>
-    </div>
+
+      <div className="container">
+        <div className="btnHaut">
+          <button
+            className="moins"
+            onClick={() => {
+              setCounter(counter - 1);
+            }}
+          >
+            <img
+              style={counter === 0 && { display: "none" }}
+              src={Moins}
+              alt="button moins"
+            />
+          </button>
+          <span>{counter}</span>
+          <button
+            className="plus"
+            onClick={() => {
+              setCounter(counter + 1);
+            }}
+          >
+            <img
+              style={counter > 10 && { display: "none" }}
+              src={Plus}
+              alt="button plus"
+            />
+          </button>
+        </div>
+        <div className="btnBas">
+          <button className="reset" onClick={reset}>
+            Reset
+          </button>
+        </div>
+      </div>
+      <footer></footer>
+    </>
   );
-}
+};
 
 export default App;
